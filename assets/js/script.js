9885579499ffd91e487ac3c5279f259ac0f58281
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-23 15:39:45 -0500

    added api call for putting together food object

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var baseCocktailurl = "https://www.thecocktaildb.com/api/json/v1/1/";
 var baseMealUrl = "https://www.themealdb.com/api/json/v1/1/";
 
 //cocktail api functions
+// ---------------------- //
 //gets random cocktail id
 function getCocktail(alcoholic) {
   fetch(baseCocktailurl + "filter.php?a=" + alcoholic)
@@ -46,6 +47,7 @@ function getCocktailInformation(drinkId) {
           drinkObj.ingredientMeasurements.push(drink[measurement]);
         }
       }
+      //call render function with (drinkObj) as parameter
       console.log(drinkObj);
     });
 }
@@ -55,7 +57,10 @@ function getCocktailInformation(drinkId) {
 getCocktail("Non_Alcoholic");
 
 //meal api functions
+// ---------------------- //
 
+//dessert or main dish can be retrieved with this function
+//for dessert pass in "Dessert" as the type
 function getFoodItemId(type) {
   //fetches data for if type is entered
   fetch(baseMealUrl + "filter.php?c=" + type)
@@ -65,7 +70,7 @@ function getFoodItemId(type) {
     .then(function (data) {
       if (data.meals) {
         var item = data.meals[Math.floor(Math.random() * data.meals.length)];
-        console.log(item.strMeal);
+        getFoodObject(item.idMeal);
       } else {
         //fetches data for if area is entered
         fetch(baseMealUrl + "filter.php?a=" + type)
@@ -81,4 +86,41 @@ function getFoodItemId(type) {
     });
 }
 
+//creates meal or dessert object from id
+function getFoodObject(foodItemId) {
+  fetch(baseMealUrl + "lookup.php?i=" + foodItemId)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var food = data.meals[0];
+      var foodObj = {
+        id: food.idMeal,
+        name: food.strMeal,
+        ingredientMeasurements: [],
+        ingredients: [],
+        steps: food.strInstructions,
+        imgSrc: food.strMealThumb,
+      }
+      //get list of food ingredients
+      for (let i = 1; i <= 15; i++) {
+        var ingredient = "strIngredient" + i;
+        if (food[ingredient]) {
+          foodObj.ingredients.push(food[ingredient]);
+        }
+      }
+      //gets list of ingredient measurements
+      for (let i = 1; i <= 15; i++) {
+        var measurement = "strMeasure" + i;
+        if (food[measurement]) {
+          foodObj.ingredientMeasurements.push(food[measurement]);
+        }
+      }
+
+      //call render function with (foodObj) as parameter
+      console.log(foodObj);
+    });
+}
+
+//test function call. should execute on button click
 getFoodItemId("Seafood");
