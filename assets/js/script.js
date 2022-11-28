6175009b7705d4c32ab9f0194d86472b5fa3fc54
Matrix <rhailu88@gmail.com>
2022-11-28 11:22:30 -0500

    Add save button to script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,13 @@
-var baseCocktailurl = "https://www.thecocktaildb.com/api/json/v1/1/";
+var storedItem = localStorage.getItem("storedItem");
 
-//gets random cocktail id
-function getCocktail(alcoholic) {
-  fetch(baseCocktailurl + "filter.php?a=" + alcoholic)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //gets random drink
-      var drink = data.drinks[Math.floor(Math.random() * data.drinks.length)];
-      var drinkId = drink.idDrink;
-      getCocktailInformation(drinkId);
-      console.log(drinkId);
-    });
+function save(){
+  var Item = document.getElementById("input").value;
+  localStorage.setItem("storedItem", Item);
+  document.getElementById("savedtext").innerHTML = Item + "SAVED";
+
 }
+function get(){
+  localStorage.getItem("storedItem");
+  document.getElementById("openedtext").innerHTML = storedItem + "OPENED";
 
-//creates cocktail object from cocktail id
-function getCocktailInformation(drinkId) {
-  fetch(baseCocktailurl + "/lookup.php?i=" + drinkId)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var drink = data.drinks[0];
-      var drinkObj = {
-        id: drink.idDrink,
-        name: drink.strDrink,
-        ingredientMeasurements: [],
-        ingredients: [],
-        steps: drink.strInstructions,
-      };
-      //gets list of drink ingredents
-      for (let i = 1; i <= 15; i++) {
-        var ingredient = "strIngredient" + i;
-        if (drink[ingredient]) {
-          drinkObj.ingredients.push(drink[ingredient]);
-        }
-      }
-      //gets list of ingredient measurements
-      for (let i = 1; i <= 15; i++) {
-        var measurement = "strMeasure" + i;
-        if (drink[measurement]) {
-          drinkObj.ingredientMeasurements.push(drink[measurement]);
-        }
-      }
-      console.log(drinkObj);
-    });
 }
-
-
-//executed on button click. 
-//value of alcoholic or non alcoholic should be passed in
-getCocktail("Non_Alcoholic");
