5bca6fca1ab020ee86da6a5564b05b405ecdbd71
dbedrossian <danny.bedrossian@gmail.com>
2022-11-28 11:43:01 -0500

    fixed some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,6 +199,7 @@ function renderDinnerRecipe(foodObj) {
         li.setAttribute("favorites", i);
 
         var button = document.createElement("button");
+        button.classList.add("button", "is-white")
         button.textContent = favorite.name;
 
         li.appendChild(button);
